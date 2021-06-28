df402fd9d5083b526ac5325566ceefc9f87e37f0
Abhishek Tomar <itsabhi.at@gmail.com>
2021-06-28 13:49:17 +0530

    2.AboutSection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// import pages
+import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,6 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+const AboutUs = () => {
+  return <AboutSection />;
+};
+export default AboutUs;
